192f4a0cadbec84406440ac56d1890b2c97b5a7f
Vishwanath <vishwasjce@gmail.com>
2025-08-18 20:48:12 -0400

    Initial commit - OrderMicroservice

---
+++ DataAccessLayer/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+
+
+
+namespace eCommerce.DataAccessLayer.Context;
+
+public class ApplicationDbContext : DbContext
+{
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+  {
+  }
+
+  public DbSet<Product> Products { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+
+
+  }
+}
